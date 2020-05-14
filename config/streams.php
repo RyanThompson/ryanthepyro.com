6ad995c30f187a194ffce071461dca190ac2eda7
Ryan Thompson <service@aiwebsystems.com>
2020-05-14 13:42:56 -0500

    Initial commit

---
+++ config/streams.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | System
+    |--------------------------------------------------------------------------
+    |
+    | Register system level configuration.
+    |
+    */
+
+    'installed' => env('INSTALLED', false),
+
+    /*
+    |--------------------------------------------------------------------------
+    | View Overrides
+    |--------------------------------------------------------------------------
+    |
+    | Define globally overridden views as 'view' => 'override' view paths.
+    |
+    */
+
+    'overrides' => [],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Control Panel Customization
+    |--------------------------------------------------------------------------
+    |
+    | Support for control panel configuration is
+    | currently limited to the Streams module.
+    |
+    */
+
+    'cp' => [],
+
+];
---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+use Anomaly\Streams\Platform\Ui\Table\TableBuilder;
+
+Route::view('/', 'welcome');
+
+Route::any('/test', function () {
+
+    $table = (new TableBuilder([
+        'stream' => 'plants',
+        'filters' => [
+            'name',
+            'type',
+        ],
+        'columns' => [
+            'name',
+            'type',
+        ],
+        'buttons' => [
+            'edit',
+        ],
+    ]));
+
+    return $table->render();
+
+    $stream = app('streams::plants');
+
+    return View::parse('
+    <ul>
+        @foreach($stream->repository()->all() as $entry)
+        <li><a href="/garden/{{ $entry->id }}">{{ $entry->name }} <small>({{ $entry->life_cycle }})</small></a></li>
+        @endforeach
+    </ul>
+    ', compact('stream'));
+});
+
+Route::get('/garden', function () {
+
+    $stream = app('streams::plants');
+
+    return View::parse('
+    <ul>
+        @foreach($stream->repository()->all() as $entry)
+        <li><a href="/garden/{{ $entry->id }}">{{ $entry->name }} <small>({{ $entry->life_cycle }})</small></a></li>
+        @endforeach
+    </ul>
+    ', compact('stream'));
+});

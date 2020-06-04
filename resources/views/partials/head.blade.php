c20728da238c1aa54d444a72c8d18c84ffa62d84
Ryan Thompson <ryan@pyrocms.com>
2020-06-04 12:27:32 -0500

    Eh

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>{{ config('app.name') }}</title>
+
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+<!-- Styles -->
+{!! assets()->style('public::css/app.css') !!}
